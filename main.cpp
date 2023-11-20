4ade048b98fff60a0dc468c9c220a0d457234a96
moganda12 <115957608+moganda12@users.noreply.github.com>
2023-11-20 03:32:45 +0000

    first commit

---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cmath>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+using namespace this_thread;
+using namespace chrono;
+
+using str = string;
+
+int main() {
+    str planetname = "I don't know yet";
+    str mountainname = "Mt. Cheesecutter";
+    cout << "On the planet of, " << planetname << ',' << flush;
+    sleep_for(seconds(1));
+    cout << " goblins come in many diffent colors!\n";
+    sleep_for(seconds(2));
+    cout << "But most of the goblins are red or blue. " << flush;
+    sleep_for(seconds(1));
+    cout << "Red and blue goblins hate each other\n";
+    sleep_for(seconds(2));
+    cout << "But one day a yellow goblin was born, and their name is...\n";
+    str name;
+    cout << "Name: ";
+    getline(cin >> ws, name);
+    sleep_for(seconds(2));
+    array<str, 3> pronouns;
+    char pronoun;
+    cout << "Pronouns: he/him[h] she/her[s]  they/them[t]\nPronouns:";
+    cin >> pronoun;
+    switch (pronoun) {
+        case 'h':
+        case 'H':
+            pronouns[0] = "he";
+            pronouns[1] = "him";
+            pronouns[2] = "his";
+            break;
+        case 's':
+        case 'S':
+            pronouns[0] = "she";
+            pronouns[1] = "her";
+            pronouns[2] = "her";
+            break;
+        case 't':
+        case 'T':
+            pronouns[0] = "they";
+            pronouns[1] = "them";
+            pronouns[2] = "their";
+            break;
+        default:
+            pronouns[0] = "he";
+            pronouns[1] = "him";
+            pronouns[2] = "his";
+            break;
+    }
+    cout << "Ok " << pronouns[2] << " name is " << name << ".\n";
+    return 0;
+}
---
+++ playerUtils/calc.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+typedef string str;
+
+int main() {
+    bool running = true;
+    char operand;
+    float num1, num2, result;
+
+    while (running) {
+        cout << "Enter an operation (+, -, *, /): ";
+        cin >> operand;
+
+        cout << "Enter two numbers: ";
+        cin >> num1 >> num2;
+
+        switch (operand) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                cout << "Invalid operation!\n";
+                running = false;
+                break;
+        }
+
+        cout << "Result: " << result << '\n';
+
+        cout << "Continue? [Y/N]: ";
+        cin >> operand;
+        switch (operand) {
+            case 'Y':
+            case 'y':
+                running = true;
+                break;
+            case 'N':
+            case 'n':
+                running = false;
+                break;
+            default:
+                cout << "Invalid input!\n";
+                break;
+        }
+    }
+}
